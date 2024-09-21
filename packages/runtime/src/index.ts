4d19f4d33b0198555bf43f43ce469120edeaed70
Jack van der Bilt <jack@managing.software>
2024-09-21 15:28:45 +0200

    fix(runtime): append style element to body instead of head

--- packages/runtime/src/index.ts
+++ packages/runtime/src/index.ts
@@ -3,4 +3,4 @@ import { generate } from '@cssified-html/core'
 const css = generate( document.documentElement.outerHTML )
 const styleNode = document.createElement( 'STYLE' )
 styleNode.innerText = css
-document.querySelector( 'html > head' )?.append( styleNode )
+document.querySelector( 'html > body' )?.append( styleNode )
